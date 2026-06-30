b151d59512ad9f5078a008ce0b608d7daf7d7d7c
vikashyadav1815 <vikashyadav1815@gmail.com>
2026-06-30 11:07:34 +0530

    add new font for heading as well as for content

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,27 @@
-import { Syne, Karla } from "next/font/google";
+// import { Syne, Karla } from "next/font/google";
+import { Montserrat, Poppins } from "next/font/google";
 import "./globals.css";
 
-const syne = Syne({
-  variable: "--font-syne",
+// const syne = Syne({
+//   variable: "--font-syne",
+//   subsets: ["latin"],
+// });
+
+// const karla = Karla({
+//   variable: "--font-karla",
+//   subsets: ["latin"],
+// });
+
+const montserrat = Montserrat({
   subsets: ["latin"],
+  variable: "--font-heading",
+  weight: ["400", "500", "600", "700", "800"],
 });
 
-const karla = Karla({
-  variable: "--font-karla",
+const poppins = Poppins({
   subsets: ["latin"],
+  variable: "--font-body",
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata = {
@@ -21,7 +34,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${syne.variable} ${karla.variable} font-sans bg-white text-zinc-900 antialiased`}
+        className={`${montserrat.variable} ${poppins.variable} font-sans bg-white text-zinc-900 antialiased`}
+        // className={`${syne.variable} ${karla.variable} font-sans bg-white text-zinc-900 antialiased`}
       >
         {children}
       </body>
